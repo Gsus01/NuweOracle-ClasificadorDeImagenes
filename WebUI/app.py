c942e5bca753e5874e910ad526693b61ca84a888
Jesus Palomino <jesus.palominoabreu@gmail.com>
2023-03-20 17:22:25 +0100

    error en app.py

--- WebUI/app.py
+++ WebUI/app.py
@@ -32,4 +32,4 @@ def upload_file():
             return jsonify({'prediction': prediction})
 
 if __name__ == '__main__':
-    app.run(debug=True, port=80, )
+    app.run(debug=True, port=80, host="0.0.0.0")
